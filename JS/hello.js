d63f444d55989b88cd3fe8cb853a9cc9ffb4b15f
Alhagie Jobe <aeojobe@gmail.com>
2019-03-28 19:39:30 -0400

    added comments

--- JS/hello.js
+++ JS/hello.js
@@ -1,3 +1,4 @@
+//this function prints a welcome message on page index.html
 function say(greeting){
     document.write(greeting);  
 }
--- JS/sample.js
+++ JS/sample.js
@@ -1,3 +1,5 @@
+//This function prints a message: 'This is just sample data '
+//on my income, expense, and production trackers.
 function Message() {
     document.write("This is just sample data.")
 }
